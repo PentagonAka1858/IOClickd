a5907a081d1b94ba6873146447f1c9a84a68a04f
Adrián Nieto <adrian.niezam@protonmail.com>
2026-05-26 21:26:55 +0200

    feature: middleware admin/moderador

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,7 +7,17 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',    [AuthController::class, 'login']);
 
-// Rutas protegidas
+// Rutas protegidas por Auth solo
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+// Solo ADMIN
+Route::middleware(['auth:sanctum', 'rol:ADMIN'])->group(function () {
+    
+});
+
+// ADMIN o MOD
+Route::middleware(['auth:sanctum', 'rol:ADMIN,MOD'])->group(function () {
+    
 });
